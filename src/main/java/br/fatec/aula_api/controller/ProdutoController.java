01f38301cdb5d9d691bf03415aa6a0a376a2dbe2
Fabio Faria <fabio.faria@luizalabs.com>
2025-05-26 19:08:56 -0300

    feat: add products route

--- src/main/java/br/fatec/aula_api/controller/ProdutoController.java
+++ src/main/java/br/fatec/aula_api/controller/ProdutoController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.fatec.aula_api.model.Produto;
@@ -16,26 +18,19 @@ public class ProdutoController {
 	@GetMapping(path = "/produtos")
 	public List<Produto> getProdutos() {
 		
-		Produto p = new Produto();
-		p.setCodigo(9393);
-		p.setDescricao("notebook");
-		p.setMarca("asus");
-		p.setEstoque(9);
-		p.setPrecoCusto(2500.99);
-		p.setPrecoVenda(3199.99);
-				
-		Produto p2 = new Produto();
-		p2.setCodigo(333);
-		p2.setDescricao("Geladeira");
-		p2.setEstoque(3);
-		p2.setMarca("Brastemp");
-		p2.setPrecoCusto(2500.0);
-		p2.setPrecoVenda(2999.99);
-		
-		listaProdutos.add(p2);
-		listaProdutos.add(p);
-		
 		return this.listaProdutos;
+	}
+	
+	@PostMapping(path = "/produtos")
+	public Produto cadastrarProduto(@RequestBody Produto produto) {
+		
+		//gera o código aleatório para o produto a ser cadastrado
+		Integer codigo = (int) (Math.random() * 1000);
+		produto.setCodigo(codigo);
+		
+		this.listaProdutos.add(produto);
+		
+		return produto;
 	}
 
 }
